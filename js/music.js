bdf6113291c22fdb44cbfd2c6bbfe213d8f033ae
tatsuki-github <t-tsuchiyama@ilab.pu-kumamoto.ac.jp>
2021-08-28 15:17:36 +0900

    Update music.js

--- js/music.js
+++ js/music.js
@@ -18,6 +18,16 @@ var note2;
 var note3;
 var note4;
 
+note_do = new Audio("sound/note_do.mp3");
+note_re = new Audio("sound/note_re.mp3");
+note_mi = new Audio("sound/note_mi.mp3");
+note_fa = new Audio("sound/note_fa.mp3");
+note_so = new Audio("sound/note_so.mp3");
+note_ra = new Audio("sound/note_ra.mp3");
+note_shi = new Audio("sound/note_shi.mp3");
+note_do2 = new Audio("sound/note_do2.mp3");
+
+
 
     function changeCategory1() {
 
@@ -25,35 +35,35 @@ var note4;
 
         switch (changedCategory1){
             case "do":
-                note1 = new Audio("note_do.mp3");
+                note1 = note_do;
                 document.getElementById("1").innerText = "ド";            
                 break;
             case "re":
-                note1 = new Audio("note_re.mp3");
+                note1 = note_re;
                 document.getElementById("1").innerText = "レ";              
                 break;
             case "mi":
-                note1 = new Audio("note_mi.mp3");
+                note1 = note_mi;
                 document.getElementById("1").innerText = "ミ";
                 break
             case "fa":
-                note1 = new Audio("note_fa.mp3");
+                note1 = note_fa;
                 document.getElementById("1").innerText = "ファ";
                 break
             case "so":
-                note1 = new Audio("note_so.mp3");
+                note1 = note_so;
                 document.getElementById("1").innerText = "ソ";
                 break
             case "ra":
-                note1 = new Audio("note_ra.mp3");           
+                note1 = note_ra;           
                 document.getElementById("1").innerText = "ラ";
                 break
             case "shi":
-                note1 = new Audio("note_shi.mp3");
+                note1 = note_shi;
                 document.getElementById("1").innerText = "シ";
                 break
             case "do2":
-                note1 = new Audio("note_do2.mp3");             
+                note1 = note_do2;             
                 document.getElementById("1").innerText = "ド";
                 break            
             default:               
@@ -65,35 +75,35 @@ var note4;
 
         switch (changedCategory2){
             case "do":
-                note2 = new Audio("note_do.mp3");
+                note2 = note_do;
                 document.getElementById("2").innerText = "ド";            
                 break;
             case "re":
-                note2 = new Audio("note_re.mp3");
+                note2 = note_re;
                 document.getElementById("2").innerText = "レ";              
                 break;
             case "mi":
-                note2 = new Audio("note_mi.mp3");
+                note2 = note_mi;
                 document.getElementById("2").innerText = "ミ";
                 break
             case "fa":
-                note2 = new Audio("note_fa.mp3");
+                note2 = note_fa;
                 document.getElementById("2").innerText = "ファ";
                 break
             case "so":
-                note2 = new Audio("note_so.mp3");
+                note2 = note_so;
                 document.getElementById("2").innerText = "ソ";
                 break
             case "ra":
-                note2 = new Audio("note_ra.mp3");           
+                note2 = note_ra;           
                 document.getElementById("2").innerText = "ラ";
                 break
             case "shi":
-                note2 = new Audio("note_shi.mp3");
+                note2 = note_shi;
                 document.getElementById("2").innerText = "シ";
                 break
             case "do2":
-                note2 = new Audio("note_do2.mp3");             
+                note2 = note_do2;             
                 document.getElementById("2").innerText = "ド";
                 break            
             default:               
@@ -106,35 +116,35 @@ var note4;
 
         switch (changedCategory3){
             case "do":
-                note3 = new Audio("note_do.mp3");
+                note3 = note_do;
                 document.getElementById("3").innerText = "ド";            
                 break;
             case "re":
-                note3 = new Audio("note_re.mp3");
+                note3 = note_re;
                 document.getElementById("3").innerText = "レ";              
                 break;
             case "mi":
-                note3 = new Audio("note_mi.mp3");
+                note3 = note_mi;
                 document.getElementById("3").innerText = "ミ";
                 break
             case "fa":
-                note3 = new Audio("note_fa.mp3");
+                note3 = note_fa;
                 document.getElementById("3").innerText = "ファ";
                 break
             case "so":
-                note3 = new Audio("note_so.mp3");
+                note3 = note_so;
                 document.getElementById("3").innerText = "ソ";
                 break
             case "ra":
-                note3 = new Audio("note_ra.mp3");           
+                note3 = note_ra;           
                 document.getElementById("3").innerText = "ラ";
                 break
             case "shi":
-                note3 = new Audio("note_shi.mp3");
+                note3 = note_shi;
                 document.getElementById("3").innerText = "シ";
                 break
             case "do2":
-                note3 = new Audio("note_do2.mp3");             
+                note3 = note_do2;             
                 document.getElementById("3").innerText = "ド";
                 break            
             default:               
@@ -147,35 +157,35 @@ var note4;
 
         switch (changedCategory4){
             case "do":
-                note4 = new Audio("note_do.mp3");
+                note4 = note_do;
                 document.getElementById("4").innerText = "ド";            
                 break;
             case "re":
-                note4 = new Audio("note_re.mp3");
+                note4 = note_re;
                 document.getElementById("4").innerText = "レ";              
                 break;
             case "mi":
-                note4 = new Audio("note_mi.mp3");
+                note4 = note_mi;
                 document.getElementById("4").innerText = "ミ";
                 break
             case "fa":
-                note4 = new Audio("note_fa.mp3");
+                note4 = note_fa;
                 document.getElementById("4").innerText = "ファ";
                 break
             case "so":
-                note4 = new Audio("note_so.mp3");
+                note4 = note_so;
                 document.getElementById("4").innerText = "ソ";
                 break
             case "ra":
-                note4 = new Audio("note_ra.mp3");           
+                note4 = note_ra;           
                 document.getElementById("4").innerText = "ラ";
                 break
             case "shi":
-                note4 = new Audio("note_shi.mp3");
+                note4 = note_shi;
                 document.getElementById("4").innerText = "シ";
                 break
             case "do2":
-                note4 = new Audio("note_do2.mp3");             
+                note4 = note_do2;             
                 document.getElementById("4").innerText = "ド";
                 break            
             default:               
